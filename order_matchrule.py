af436d80e057f9ac33659231fabe4a80278dd85c
Michael M <m.migsch@gmail.com>
2019-10-24 10:43:19 +0200

    Create order_matchrule.py

---
+++ order_matchrule.py
@@ -0,0 +1,28 @@
+# Description: Script orders the three parts of the matchrule.
+# Version: 0.1
+# Author: MiMi
+
+def order_matchrule(matchrule, *args):
+    """General: Prints a string in order of *args.
+        Works only for oneline matchrules.
+
+        Parameters: *args takes three indexes in the order the result is wanted."""
+    
+    mr = matchrule.split("|")
+    res = []
+    try:
+        if len(args) == 3:
+            for arg in args:
+                res.append(mr[int(arg)])
+    except:      
+        print("Too much arguments only three indexes required (0, 1, 2).")
+        matchrule = input("Enter the matchrule: ")
+        args = input("Enter the three indexes without any spaces: ")
+        order_matchrule(matchrule, *args)
+        
+    print("|".join(res))
+
+matchrule = input("Enter the matchrule: ")
+args = input("Enter the three indexes without any spaces: ")
+order_matchrule(matchrule, *args)
+
