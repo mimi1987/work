3b7724b1c4776d2807d6a78c72566e7a1d8cbb16
Michael M <m.migsch@gmail.com>
2019-10-25 10:51:14 +0200

    Update order_matchrule.py

--- order_matchrule.py
+++ order_matchrule.py
@@ -1,27 +1,23 @@
 # Description: Script orders the three parts of the matchrule.
-# Version: 0.1
+# Version: 0.5
 # Author: MiMi
 
 from pyperclip import copy
 
-def order_matchrule(matchrule, *args):
+def order_matchrule(matchrule, indices):
     """General: Prints a string in order of *args.
         Works only for oneline matchrules.
-
         Parameters: *args takes three indices in the order the result is wanted."""
     
     mr = matchrule.split("|")
     res = []
+    print(indices)
     try:
-        if len(args) <= 3:
-            for arg in args:
-                res.append(mr[int(arg)])
+        if len(indices) <= 3:
+            for i in indices:
+                res.append(mr[int(i)])
     except:      
         print("Too much arguments only three indices required (0, 1, 2).")
-    else:
-        matchrule = input("Enter the matchrule: ")
-        args = input("Enter the three indices without any spaces: ")
-        order_matchrule(matchrule, *args)
     mr_result = "|".join(res)    
     print(mr_result)
     copy(mr_result)
@@ -29,5 +25,6 @@ def order_matchrule(matchrule, *args):
 
 while True:
     matchrule = input("Enter the matchrule: ")
-    args = input("Enter the three indices without any spaces: ")
-    order_matchrule(matchrule, *args)
+    indices = tuple(input("Enter the three indices without any spaces: "))
+    print(indices)
+    order_matchrule(matchrule, indices)
