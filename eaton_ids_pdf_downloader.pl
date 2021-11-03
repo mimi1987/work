d3755abb1a1503d060800442dbe7c0b8b91dc436
Michael M <m.migsch@gmail.com>
2021-11-03 17:56:31 +0100

    Update eaton_ids_pdf_downloader.pl

--- eaton_ids_pdf_downloader.pl
+++ eaton_ids_pdf_downloader.pl
@@ -1,17 +1,27 @@
 use v5.30.0;
-use LWP::Simple;
 use IO::Handle;
+use LWP::UserAgent;
+use HTTP::Request;
 
 # Shows the output every second in the loop when using sleep.
 STDOUT->autoflush(1);
 
+# Create and set UserAgent object
+my %options = (
+  agent => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:53.0) Gecko/20100101 Firefox/53.0"
+);
+my $browser = LWP::UserAgent->new(%options);
+$browser->ssl_opts(
+  verify_hostname => 0,
+);
+
 # Global Variables.
-my $read_file_path = 'C:\Users\Geizhals\Downloads\eaton_ids.txt';
-my $base_storage_path_for_pdfs = 'C:\\Users\\Geizhals\\Downloads\\';
+my $read_file_path = 'C:\\Users\\michaelm\\Downloads\\eaton_ids.txt';
+my $base_storage_path_for_pdfs = 'C:\\Users\\michaelm\\Downloads\\';
 my @IDs;
 my $download_url = 'https://datasheet.eaton.com/datasheet.php';
 my $model = '?model=';
-my $parameter = '&locale=en_GB&type=pdf';
+my $parameter = '&locale=de_DE&type=pdf';
 
 # Open the file handle READ to read the Eaton IDs.
 open READ, '<', $read_file_path or die "Couldn't open the file: $!\n";
@@ -30,9 +40,30 @@ while (my $line = <READ>)
 foreach my $ID (@IDs)
 {
   my $url = "";
+  my $file_path = "";
+
   $url = $download_url . $model . $ID . $parameter;
-  say "Downloaded: $url";
-  getstore $url, "$base_storage_path_for_pdfs" . "$ID.pdf" or warn "Download Failed!";
+  $file_path = "$base_storage_path_for_pdfs" . "$ID.pdf";
+
+  say "Download: $url";
+  say "File Path: $file_path";
+
+  my $req = HTTP::Request->new(GET => $url);
+
+  my $response = $browser->request($req);
+
+  if ($response->is_success)
+  {
+    open PDF, '>', $file_path;
+    binmode(PDF);
+    print PDF $response->content;
+    close PDF;
+  }
+  else
+  {
+    print "Error in " . $response->status_line;
+  }
+
   sleep(5);
 }
 
